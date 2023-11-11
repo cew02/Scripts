fb68b9c2bd9ea089f59805fb7c1379cf7025d17c
Cody Wiley <cwiley.cs@gmail.com>
2023-11-11 16:30:20 -0500

    Create mergelist.py

---
+++ mergelist.py
@@ -0,0 +1,14 @@
+# lists
+
+animals = ["dog", "cat", "fox", "wolf"]
+other_animals = ["Lion", "tiger", "bear"] # oh my
+
+# concatenation
+
+animal_merge = animals + other_animals
+print(animal_merge)
+
+# list method
+
+animals.extend(other_animals)
+print(animals)
