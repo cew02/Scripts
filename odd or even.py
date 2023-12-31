f0e574e44a4a482d285e6f2ab6d3d116492a83a4
Cody Wiley <cwiley.cs@gmail.com>
2023-12-31 01:20:12 -0500

    Update odd or even.py

--- odd or even.py
+++ odd or even.py
@@ -4,3 +4,12 @@ if (num % 2) == 0:
     print('{0} is even'.format(num))
 else:
     print('{0} is odd'.format(num))
+
+# .format is used to format strings
+# .format(num) 
+    
+# example: 
+    
+string = "Hello, {}"
+formatted_string = string.format('world') # you can replace "world" with any word and it will print that word.
+print(formatted_string)
