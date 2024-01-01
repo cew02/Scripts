d609f89f37a0307f60a19120bd53c4a764d8c5a2
Cody Wiley <cwiley.cs@gmail.com>
2024-01-01 00:55:24 -0500

    Update booty meme.py
    addition

--- booty meme.py
+++ booty meme.py
@@ -7,3 +7,19 @@ elif butt_size == 3:
     print('Your butt is pretty big there, why dont you come down and ill show you a good time!')
 else:
     print('WOW Your butt is pretty big! you better pull your pants down RIGHT NOW so i can lick it clean')
+
+# No unit of measurement
+    
+# Original code from post on r/learnpython
+
+'''
+butt_size = input('How big is your butt?')
+if butt_size <=2:
+    print('Your butt is too small Im not gonna smack it')
+elif butt_size == 3:
+    print('Your butt is decently sized, if im super horny ill smack and maybe lick it')
+elif butt_size == 4:
+    print('Your butt is pretty big there, why dont you come down and ill show you a good time!')
+else:
+    print('WOW Your butt is pretty big! you better pull your pants down RIGHT NOW so i can lick it clean')
+'''
