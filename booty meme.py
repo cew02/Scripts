8323d4b79bcbfe3f5211f2a93466a5cbcb85b0c1
Cody Wiley <cwiley.cs@gmail.com>
2024-01-01 00:42:39 -0500

    Create booty meme.py
    just a meme I saw on r/learnpython. Thought I'd use it to give myself some practice

---
+++ booty meme.py
@@ -0,0 +1,9 @@
+butt_size = int(input('How big is your butt?'))
+if butt_size <=1:
+    print('Your butt is too small Im not gonna smack it')
+elif butt_size == 2:
+    print('Your butt is decently sized, if im super horny ill smack and maybe lick it')
+elif butt_size == 3:
+    print('Your butt is pretty big there, why dont you come down and ill show you a good time!')
+else:
+    print('WOW Your butt is pretty big! you better pull your pants down RIGHT NOW so i can lick it clean')
