633f6cd6f5d826c5a84f7848c8c7b6968acdf5c7
Cody Wiley <cwiley.cs@gmail.com>
2024-01-03 15:28:57 -0500

    organization + lua = fun times

---
+++ Lua/learn.lua
@@ -0,0 +1,8 @@
+-- You thought this was going to be just a python practice repository? Think again...
+
+-- going to try reprogramming my coin.py code in lua
+
+local randomNumber = getRandomNumber(1, 100)
+print("Random Number: " .. randomNumber)
+
+--placement holder for now
