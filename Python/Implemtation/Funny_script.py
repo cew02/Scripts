8c9b606ce38ab4231eceaf0f5b66da5f04a56552
Cody Wiley <cwiley.cs@gmail.com>
2024-03-27 10:29:44 -0400

    Update Funny_script.py

--- Python/Implemtation/Funny_script.py
+++ Python/Implemtation/Funny_script.py
@@ -2,11 +2,10 @@ import random
 
 user = int(input('Do you want me to pick a random number? 1. Yes      2. No'))
 
-while True:
-    if user == 1:
-        print('Yipee!')
-        print(random.randrange(1,1999999999))
-    else:
-        print('W-w-w-why not? ;~;')
-        input()
-        print('Oh, ok then. I will leave you alone. have a good life.')
+if user == 1:
+    print('Yipee!')
+    print(random.randrange(1,1999999999))
+else:
+    print('W-w-w-why not? ;~;')
+    input()
+    print('Oh, ok then. I will leave you alone. have a good life.')
